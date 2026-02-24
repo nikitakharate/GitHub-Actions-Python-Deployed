5e7406e2c182a52b336e1f038d55472b4d9e12b7
nikitakhrate <kharatenikita305@gmail.com>
2026-02-24 23:08:48 +0530

    added

---
+++ app.py
@@ -0,0 +1,10 @@
+with open("index.html", "w") as f:
+    f.write("""
+    <html>
+        <head><title>CI/CD Deployment</title></head>
+        <body>
+            <h1>Welcome</h1>, 
+            <h2>to CI/CD pipeline deployed on GitHub Actions Using Python</h2>
+        </body>
+    </html>
+    """)
